bbb2d1d624d1144b33a7934b1df96dd79c4d24a6
ziraldodiego <ziraldodiego@gmail.com>
2020-03-24 00:35:30 -0300

    a

---
+++ HolaMundo/js/33-json.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//JSON - JavaScript Object Notation
+
+//Aqui estamos creando un objeto JSON
+//El objeto JSON, es un array asociativo, 
+//adentro va a tener propiedades, y las propiedades sus valores
+var pelicula = {
+    titulo: 'Batman vs Superman',
+    year: 2017,
+    pais: 'Estados Unidos' 
+}
+
+//Aqui estamos creando un Array de objetos JSON
+var peliculas = [
+    {
+    titulo:'Esperando la carroza',
+    year: 1985,
+    pais: 'Argentina'
+},
+    {
+    titulo:'Los Exterminator',
+    year: 1990,
+    pais: 'Argentina'
+
+    }
+]
+
+/* pelicula.titulo = "Superman Begins" */
+
+console.log(peliculas)
+
